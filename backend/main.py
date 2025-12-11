896285806346d561fc8218b58d24469514f99dc7
Mohak-Singhal <mohaksinghal516@gmail.com>
2025-12-11 22:19:15 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.resume_routes import router as resume_router
 
 app = FastAPI()
 
+# CORS settings
+origins = [
+    "http://localhost:3000", 
+    "https://interviewer-ashy.vercel.app/"  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],     
+    allow_headers=["*"],     
+)
+
+# Include your router
 app.include_router(resume_router, prefix="/resume", tags=["Resume Parsing"])
